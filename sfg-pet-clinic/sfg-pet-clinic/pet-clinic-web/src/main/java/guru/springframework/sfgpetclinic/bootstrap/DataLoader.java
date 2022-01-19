120c0f09afa770ad17fa8f325a8da4fe340e1441
cjh1988 <chris.sk8er.1@gmail.com>
2022-01-19 18:43:15 -0300

    Update Map Impl to Manage setting of ID Property, closes #14 closes #22

--- sfg-pet-clinic/sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,13 +23,13 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setFirstName("Christian");
         owner1.setLastName("JHirache");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("Milu");
         owner2.setLastName("JHirache");
         ownerService.save(owner2);
@@ -37,13 +37,13 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners.....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setFirstName("Kassandra");
         vet1.setLastName("JHirache");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+
         vet2.setFirstName("Leonardo");
         vet2.setLastName("JHirache");
         vetService.save(vet2);
